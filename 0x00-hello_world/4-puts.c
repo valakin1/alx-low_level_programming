6b2f23a9daadfc5a7501bd7a37bc5d4d6fddb6cb
= <akinkuoroyevalentine@gmail.com>
2022-09-10 08:28:08 -0700

    Task 1 - 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: using the main function this 
+ * program prints "Programming is like building  a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ *  main - The Entry point
+ *
+ *  Description: Using the main function this
+ *  program prints "with proper grammar, but the outcome is a piece of art,"
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
