3dbde801dd8de88d1f4c224b6cc7d67ea641a4b1
= <akinkuoroyevalentine@gmail.com>
2022-09-22 10:27:26 -0700

    Advanced Task 1

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - encode string using rot13
+ * rot13 - encodes a string into rot13
  * @s: string to encode
- * Return: encoded string
+ *
+ * Return: address of s
  */
-
-char *rot13(char *);
+char *rot13(char *s)
 {
 	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
+	for (i = 0; *(s + i); i++)
 	{
-		i = 0;
-		while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
-			i++;
-
-		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
-			s[j] = b[i];
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
 	}
-
 	return (s);
 }
