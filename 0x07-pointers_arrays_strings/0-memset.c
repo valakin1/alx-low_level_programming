40ef958eacbe7a1731ff0773246b1552bcf29688
= <akinkuoroyevalentine@gmail.com>
2022-09-27 09:31:16 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,45 +1,20 @@
 #include "main.h"
-
 /**
- * _memset - fill n bytes of memory with a constant value
- * @s: pointer to memory area
- * @b: constant value
- * @n: number of bytes to fill
- * Return: memory area
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i = 0;
 
-	int size = n; /* only accept positive sizes */
-
-	if (size > 0)
+	while (i < n)
 	{
-		int i;
-
-		for (i = 0; i < size; i++)
-			s[i] = b;
+		*(s + i) = b;
+		i++;
 	}
-
 	return (s);
 }
-
-/* pointer arithmetic version
-
-   
-char *_memset(char *s, char b, unsigned int n)
-{
-
-	int size = n;
-
-	if (size > 0)
-	{
-		int i;
-
-		for (i = 0; i < size; i++)
-			*(s + i) = b;
-	}
-
-	return (s);
-}
-*/
