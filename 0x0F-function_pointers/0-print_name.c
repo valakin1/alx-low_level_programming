adc57d2a10478adf82b51ab86a45563dda0d9ea7
= <akinkuoroyevalentine@gmail.com>
2022-10-12 23:24:42 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 
 /**
  * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing functions
+ * @name: pointer to name
+ * @f: function pointer.
+ * Return: void.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
