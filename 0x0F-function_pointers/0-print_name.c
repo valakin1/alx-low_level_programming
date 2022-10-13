51566593264a5f25d09581489dddfbd968126a89
= <akinkuoroyevalentine@gmail.com>
2022-10-12 23:59:07 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 /**
- * print_name - prints a name
+ * print_name - prints a name.
  * @name: pointer to name
  * @f: function pointer.
  *
  * Return: void.
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char*))
 {
 	if (name && f)
 		f(name);
