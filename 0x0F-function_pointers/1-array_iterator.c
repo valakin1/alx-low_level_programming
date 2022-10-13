15f4ba84c3c4a31365a92ec15ea9e2a97e0ec0f8
= <akinkuoroyevalentine@gmail.com>
2022-10-12 23:54:22 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
-#include "funtion_pointers.h"
-
 /**
  * array_iterator - execute function on an array via a function ptr
- * @array: array
+ * @array: array of elements.
  * @size: size of array
- * @action: pointer to function that we need to execute
+ * @action: function pointer.
+ *
+ * Return: void.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!array || !action)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && size > 0 && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
