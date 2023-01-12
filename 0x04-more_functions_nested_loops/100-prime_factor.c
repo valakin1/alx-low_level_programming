e9218395691ad3e1f3134695d483d09a8f955dc8
= <akinkuoroyevalentine@gmail.com>
2023-01-12 07:38:33 -0800

    Tasks

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - print largest prime factor of 612852475143
- * Return: 0 on success
+ * main - prints largest prime factor.
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	unsigned long num = 612852475143;
-	unsigned long divisor = 2;
-	
-	while (divisor < num)
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
 	{
-		if (num % divisor == 0)
+		if (n % fp == 0)
 		{
-			num /= divisor;
-			divisor = 2;
+			n /= fp;
+			fp--;
 		}
-		else
-			divisor++;
 	}
-	printf("%lu\n", num);
+	printf("%ld\n", fp);
 	return (0);
 }
